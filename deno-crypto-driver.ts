3cbc7b3f809b691eefd5bb04a790bcb24d8738ec
DevJeff <45824907+DevChanQ@users.noreply.github.com>
2024-08-21 15:47:52 +0800

    Update deno-crypto-driver.ts

--- deno-crypto-driver.ts
+++ deno-crypto-driver.ts
@@ -1,4 +1,4 @@
-import { crypto } from "https://deno.land/std@0.115.0/crypto/mod.ts";
+import { crypto } from "https://deno.land/std@0.224.0/crypto/mod.ts";
 
 import * as ArweaveUtils from "./utils.ts";
 
